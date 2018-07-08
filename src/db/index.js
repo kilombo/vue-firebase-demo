218d332904b05cb448d743f51a9718c3e82cc6f1
Enrique Querol <enrique.eqg@gmail.com>
2018-07-08 09:36:39 +0200

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 const config = {
-  apiKey: 'AIzaSyBBUs65_WtGZl45IPRV4XWLrmyjmcVl5nA',
+  apiKey: '',
   authDomain: 'vue-firebase-demo-ea883.firebaseapp.com',
   databaseURL: 'https://vue-firebase-demo-ea883.firebaseio.com',
-  projectId: 'vue-firebase-demo-ea883',
+  projectId: '',
   storageBucket: '',
-  messagingSenderId: '45028215185'
+  messagingSenderId: ''
 }
 const firebaseapp = firebase.initializeApp(config)
 export default firebaseapp.firestore()
