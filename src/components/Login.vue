3d803e49796294bf6967cf28afe4cadc0e0d4e68
Enrique Querol <enrique.eqg@gmail.com>
2018-07-09 12:29:16 +0200

    Google Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,6 +4,9 @@
     <input class="form-control" type="text" v-model="email" placeholder="Email"><br>
     <input class="form-control" type="password" v-model="password" placeholder="Password"><br>
     <button v-on:click="signIn" class="btn btn-primary">Connection</button>
+    <p>
+    <button class="btn btn-secondary" @click="googleLogin">Google Login</button>
+    </p>
     <p>You don't have an account ? You can
       <router-link to="/sign-up">create one</router-link>
     </p>
@@ -12,6 +15,7 @@
 
 <script>
 import firebase from 'firebase'
+const googleProvider = new firebase.auth.GoogleAuthProvider()
 export default {
   name: 'Login',
   data() {
@@ -43,6 +47,30 @@ export default {
             alert('Oops. ' + err.message)
           }
         )
+    },
+    googleLogin() {
+      firebase
+        .auth()
+        .signInWithPopup(googleProvider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          let token = result.credential.accessToken
+          // The signed-in user info.
+          let user = result.user
+          // ...
+          this.$router.replace('products')
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          let errorCode = error.code
+          let errorMessage = error.message
+          // The email of the user's account used.
+          let email = error.email
+          // The firebase.auth.AuthCredential type that was used.
+          let credential = error.credential
+          // ...
+          console.error(error);
+        })
     }
   }
 }
